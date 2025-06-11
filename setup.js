8f39648b25072add875d29ba30facd14f0f6ca4b
Jake Rudolph <jake@socratica.com>
2025-06-11 15:26:17 -0400

    code

---
+++ setup.js
@@ -0,0 +1,10 @@
+export const player = {
+  run1: "/assets/gw_1.png",
+  run2: "/assets/gw_2.png",
+  jumping: "/assets/gj.png",
+  lose: "/assets/gh.png",
+  rotate: false,
+  jumpToTheMoon: false,
+};
+
+export const theme = "night";
